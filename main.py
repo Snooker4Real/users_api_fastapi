8faf6ccb49cf3d34c9401bb35096a1ab004eddba
Snooker4real <cindanojonathan@icloud.com>
2021-12-06 04:34:50 +0100

    adding put method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import List
 from uuid import UUID
 from fastapi import FastAPI, HTTPException
-from models import Gender, User, Role
+from models import Gender, User, Role, UserUpdateRequest
 
 app = FastAPI()
 
@@ -39,7 +39,23 @@ async def register_user(user: User):
     return {"id": user.id}
 
 
-
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+        status_code=404,
+        detail=f"user with id: {user_id} doesn't exists"
+    )
 
 
 @app.delete("/api/v1/users/{user_id}")
@@ -50,5 +66,5 @@ async def delete_user(user_id: UUID):
             return
     raise HTTPException(
         status_code=404,
-        detail=f"user with id: {user_id} doesn't exist !"
+        detail=f"user with id: {user_id} doesn't exists"
     )
